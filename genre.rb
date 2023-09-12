b6deb7519d55633a30f24124b465f3152100d4cc
BenM4k <bennymakomo@gmail.com>
2023-09-12 12:55:50 +0200

    Created Class Genre

---
+++ genre.rb
@@ -0,0 +1,13 @@
+class Genre
+    attr_accessor :name, :items
+    def initialize(id, name = "")
+        @id = id
+        @name = name
+        @items = []
+    end
+
+    def add_item(item)
+        @items << item
+        item.set_genre(self)
+    end
+end
