cb8d57753d5fb878ddc5364c7ceff040a7cd6331
John Murianki <mcjthiongo@gmail.com>
2023-09-13 11:41:26 +0300

    Added book and Label Tests

--- spec/music_spec.rb
+++ spec/music_spec.rb
@@ -1,10 +1,10 @@
-# require_relative '../genre'
+require_relative '../genre'
 
-# describe MusicAlbum do
-#   context 'When creating new music' do
-#     music = MusicAlbum.new(false)
-#     it 'Should have the right properties' do
-#       expect(music).to be_instance_of(MusicAlbum)
-#     end
-#   end
-# end
+describe MusicAlbum do
+  context 'When creating new music' do
+    music = MusicAlbum.new(false)
+    it 'Should have the right properties' do
+      expect(music).to be_instance_of(MusicAlbum)
+    end
+  end
+end
