b5eb3c0d175dca54fd1148fcab0e0b35ec1aab07
John Murianki <mcjthiongo@gmail.com>
2023-09-14 17:43:23 +0300

    Add schema for label book and item

--- schema.sql
+++ schema.sql
@@ -1,3 +1,16 @@
+CREATE DATABASE catalogue_of_my_things;
+CREATE TABLE item (
+  id SERIAL PRIMARY KEY,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN DEFAULT false,
+  label_id INT,
+  author_id INT,
+  genre_id INT,
+  FOREIGN KEY (label_id) REFERENCES labels(id),
+  FOREIGN KEY (author_id) REFERENCES author(id),
+  FOREIGN KEY (genre_id) REFERENCES genres(id)
+);
+
 CREATE TABLE author(id SERIAL PRIMARY KEY, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL);
 CREATE TABLE game(id SERIAL PRIMARY KEY, publish_date DATE NOT NULL, archived BOOLEAN NOT NULL, multiplayer BOOLEAN NOT NULL, last_played_at DATE NOT NULL, genre_id INT NOT NULL, author_id INT NOT NULL, label_id INT NULL, source_id INT NOT NULL);
 ALTER TABLE author ADD COLUMN game_id INTEGER;
@@ -10,3 +23,16 @@ CREATE TABLE music(id SERIAL PRIMARY KEY, publish_date DATE NOT NULL, archived B
 ALTER TABLE genre ADD COLUMN music_id INTEGER;
 ALTER TABLE genre ADD CONSTRAINT fk_music FOREIGN KEY (music_id) REFERENCES music(id);
 ALTER TABLE music ADD CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genre(id);
+
+CREATE TABLE label (
+    id  INT GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR(100),
+    color VARCHAR(100),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE book (
+  id INT PRIMARY KEY REFERENCES items(id),
+  publisher VARCHAR(255) NOT NULL,
+  cover_state VARCHAR(50) NOT NULL
+);
